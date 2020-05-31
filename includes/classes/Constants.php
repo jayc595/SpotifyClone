8675e5df111efcdc7fa5bc44b91659560240b42d
jayc595 <jay.crocker@hotmail.co.uk>
2020-05-31 02:39:20 +0100

    Second commit

---
+++ includes/classes/Constants.php
@@ -0,0 +1,16 @@
+<?php
+  class Constants {
+    public static $passwordsDoNotMatch = "Password do not match!";
+    public static $passwordsNotAlphaNum = "Password can only contain numbers and letters";
+    public static $passwordsCharLimit = "Your password must be between 5 and 25 characters";
+    public static $emailIsInvalid = "Invalid Email";
+    public static $emailsDoNotMatch = "Your emails do not match!";
+    public static $emailTaken = "This email is already in use";
+    public static $lastnameCharLimit = "Your last name must be between 2 and 25 characters";
+    public static $firstnameCharLimit = "Your first name must be between 2 and 25 characters";
+    public static $usernameCharLimit = "Your username must be between 5 and 25 characters";
+    public static $usernameTaken = "Sorry, this username has already been used";
+
+    public static $loginFailed = "Your username or password was incorrect";
+  }
+ ?>
