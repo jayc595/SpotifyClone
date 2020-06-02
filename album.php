750db13546c2fb55c965d662a56f5a143ef0bd82
jayc595 <jay.crocker@hotmail.co.uk>
2020-06-02 15:50:02 +0100

    New commit

---
+++ album.php
@@ -0,0 +1,25 @@
+<?php include ("includes/header.php");
+if (isset($_GET['id'])) {
+  $albumId = $_GET['id'];
+
+}
+else {
+  header("Location: index.php");
+}
+
+$albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE id='$albumId'");
+$album = mysqli_fetch_array($albumQuery);
+
+$artistId = $album['artist'];
+
+$artistQuery = mysqli_query($con, "SELECT * FROM artists WHERE id ='$artistId'");
+$artist = mysqli_fetch_array($artistQuery);
+
+echo $artist['name'] . "<br>";
+echo $album['title'];
+
+?>
+
+
+
+<?php include ("includes/footer.php"); ?>
